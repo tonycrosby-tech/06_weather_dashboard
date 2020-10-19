91fc1f435143417e43b0339b91922d1fe1cdd848
tony913 <tonycrosby96@gmail.com>
2020-10-19 14:29:14 -0500

    adding more

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,20 @@ function display_c() {
 // function to create the time
 function display_ct() {
   var x = new Date();
-  document.getElementById("ct").innerHTML = x;
+  document.getElementById('ct').innerHTML = x;
   display_c();
 }
 
+var queryResult = '';
+
+var API_KEY = 'f5e25466bfa1e46ad656169c960527a3';
+
+function currentLocation() {
+  $.ajax({
+      url: "https://freegeoip.app/json/",
+      method: "GET",
+  }).then(function (response) {
+      queryResult = response.city || 'exton';
+      console.log(queryResult);
+  });
+};
