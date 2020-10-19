fc69defa0fbf3c7f69b6f8443e74860074e6091e
tony913 <tonycrosby96@gmail.com>
2020-10-19 13:44:11 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// display time
+function display_c() {
+  var refresh = 1000; // Refresh rate in milli seconds
+  mytime = setTimeout("display_ct()", refresh);
+}
+
+// function to create the time
+function display_ct() {
+  var x = new Date();
+  document.getElementById("ct").innerHTML = x;
+  display_c();
+}
+
